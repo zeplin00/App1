6474fd1f70a222a2f223c9712f4b4b0c5f54deb9
kirandor <kirandor>
2016-11-25 14:02:24 -0800

    Azure Paas Deployment

--- mService1/ms1_WebRole1/config/auth.js
+++ mService1/ms1_WebRole1/config/auth.js
@@ -4,21 +4,21 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1464374773592610', // your App ID
-        'clientSecret'  : 'fe27572f72fcc7a2d665b90e51986360', // your App Secret
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
         'profileFields' : ['id', 'email', 'gender']
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'XmpWlTTFzimjdsC1fdS8mIkKq',
-        'consumerSecret'    : 'phzqVW8v8q925eG5XBtYTHEDd3wol6D3WFs21STEmrueXRERD7',
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
         'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '959378227277-ke16hmbng7t5j1slr247bq847b0ljhu6.apps.googleusercontent.com',
-        'clientSecret'  : 'dU8FknAM3uHoA_1GdOsiprB7',
+        'clientID'      : '',
+        'clientSecret'  : '',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
--- mService1/ms1_WebRole1/server.js
+++ mService1/ms1_WebRole1/server.js
@@ -16,10 +16,31 @@ var session      = require('express-session');
 
 var configDB = require('./config/database.js');
 
+var httpsOnly = function(req, res, next) {
+
+    // This is a hack.
+    // Fix to correctly set: req.headers['x-forwarded-proto'] and req.headers['x-forwarded-for']
+
+    if(req.headers['x-arr-ssl'] || req.headers['x-iisnode-https'] === 'on') {
+      // This is an https connection. Call the next handler
+      next();
+    } else {
+      // This is an http connection. Redirect if this is not development
+      if (process.env.NODE_ENV && process.env.NODE_ENV != 'development') {
+        return res.redirect(301, 'https://' + req.get('host') + req.url);
+      } else {
+        next();
+      }
+    }
+};
+
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
+
+// Redirect http connections
+app.use(httpsOnly);
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
