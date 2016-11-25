303325b6e98350493ba9e3ec2a80cfb96c029530
kirandor <kirandor>
2016-11-24 16:00:35 -0800

    Added FB and Twitter Federation

---
+++ mService1/ms1_WebRole1/config/auth.js
@@ -0,0 +1,26 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1464374773592610', // your App ID
+        'clientSecret'  : 'fe27572f72fcc7a2d665b90e51986360', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'profileFields' : ['id', 'email', 'gender']
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'XmpWlTTFzimjdsC1fdS8mIkKq',
+        'consumerSecret'    : 'phzqVW8v8q925eG5XBtYTHEDd3wol6D3WFs21STEmrueXRERD7',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '959378227277-ke16hmbng7t5j1slr247bq847b0ljhu6.apps.googleusercontent.com',
+        'clientSecret'  : 'dU8FknAM3uHoA_1GdOsiprB7',
+        'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
+    }
+
+};
+
